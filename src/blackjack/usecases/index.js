fcc5fc944e104f397b346df1ee7e978c3175a44b
Andrés Muñoz <andres2078@gmail.com>
2026-04-17 21:17:50 -0500

    refactor: extract computer turn logic into module

--- src/blackjack/usecases/index.js
+++ src/blackjack/usecases/index.js
@@ -1,3 +1,4 @@
-export { pedirCarta } from './pedir-carta';
-export { valorCarta } from './valor-carta';
 export { crearDeck } from './crear-deck';
+export { pedirCarta } from './pedir-carta';
+export { turnoComputadora} from './turno-computadora';
+export { valorCarta } from './valor-carta';
